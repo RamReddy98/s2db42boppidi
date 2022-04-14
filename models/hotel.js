9e01b33537c91557ba26e42b7f559f51888a6ee4
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-14 01:18:40 -0500

    Create hotel.js

---
+++ models/hotel.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose") 
+const hotelSchema = mongoose.Schema({
+Hotel_name: String,
+Location: String,
+price: Number
+})
+
+module.exports = mongoose.model("hotel", hotelSchema) 
