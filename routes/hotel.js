30eb9c13f2eeb18715b9758f3f842fc8af5dc8f7
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-14 01:27:27 -0500

    Update hotel.js

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const hotel_controlers= require('../controllers/hotel');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', hotel_controlers.hotel_view_all_Page );
+
+/*router.get('/', function(req, res, next) {
   res.render('hotel', { title: 'Search Results by Hotel' });
-});
+}); */
 
 module.exports = router;
