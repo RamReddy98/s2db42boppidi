b1d1fc915ee3d53600992b4b1e7bf77524a156f9
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-14 01:16:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ mongoose.connect(connectionString,  {useNewUrlParser: true, useUnifiedTopology: 
 //Get the default connection 
 var db = mongoose.connection;
 
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hotelRouter = require('./routes/hotel');
