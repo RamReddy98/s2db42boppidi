4da7fea3f32eb4767c566eb1b8d12b4c9aef0105
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-17 16:19:45 -0500

    Update hotel.js

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -89,3 +89,17 @@ exports.hotel_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }; 
+
+// Handle a show one view with id specified by query
+exports.hotel_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await hotel.findById( req.query.id)
+    res.render('hoteldetail',
+    { title: 'hotel Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
