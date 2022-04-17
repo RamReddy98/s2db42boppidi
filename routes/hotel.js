cd6c256acc3a90e743ac1e40102f771619b3f623
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-17 16:19:40 -0500

    Update hotel.js

--- routes/hotel.js
+++ routes/hotel.js
@@ -5,4 +5,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', hotel_controlers.hotel_view_all_Page );
 
+router.get('/detail', hotel_controlers.hotel_view_one_Page);
+
 module.exports = router;
