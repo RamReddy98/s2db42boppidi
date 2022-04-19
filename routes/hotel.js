a3d8a137125f9fe8be1d40950d74e93ce9081f00
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-19 17:22:54 -0500

    Update hotel.js

--- routes/hotel.js
+++ routes/hotel.js
@@ -8,6 +8,8 @@ router.get('/', hotel_controlers.hotel_view_all_Page );
 router.get('/detail', hotel_controlers.hotel_view_one_Page);
 router.get('/create', hotel_controlers.hotel_create_Page); 
 router.get('/update', hotel_controlers.hotel_update_Page);
+router.get('/delete', hotel_controlers.hotel_delete_Page); 
+
 
 
 module.exports = router;
