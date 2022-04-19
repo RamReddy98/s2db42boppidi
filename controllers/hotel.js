9256b54e3e2273ce35fe0bb132b4d3675e517307
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-19 16:47:52 -0500

    Update hotel.js

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -117,3 +117,17 @@ exports.hotel_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.hotel_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await hotel.findById(req.query.id)
+    res.render('hotelupdate', { title: 'hotel Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
