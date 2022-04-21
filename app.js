23204b12d75518af16a671d110e0be9f7c3fd46e
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-21 14:07:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -78,25 +78,6 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser()); 
 passport.deserializeUser(Account.deserializeUser());
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
 // We can seed the collection if needed on server start
 async function recreateDB(){ 
   // Delete everything 
@@ -120,7 +101,26 @@ await hotel.deleteMany();
  console.log("Third object saved") 
  });
 
-} 
+}
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+ 
 
 let reseed = true; 
 if (reseed) { recreateDB();}
