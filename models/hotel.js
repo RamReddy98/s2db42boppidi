e0095d564263916d149c34bba5c286a7674391fe
RamReddy98 <boppidiramreddy98@gmail.com>
2022-04-25 19:19:36 -0500

    Update hotel.js

--- models/hotel.js
+++ models/hotel.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const hotelSchema = mongoose.Schema({
     Hotel_name: String,
-    Location: String,
-    price: Number
+    Location: { type: String, minLength: 1 },
+    price: { 
+        type: Number,
+        min: 10,
+        max: 100 
+    }
 })
 module.exports = mongoose.model("hotel", hotelSchema)  
